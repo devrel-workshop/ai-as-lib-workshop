08604ac0b5bf7bc9fee956cf909fd3ba3076edf4
philippart-s <wildagsx@gmail.com>
2025-01-19 18:45:45 +0100

    feat: ✏️ Python / audio sheat sheet

--- workshop/audio/speech-to-text-http.py
+++ workshop/audio/speech-to-text-http.py
@@ -7,26 +7,33 @@ import gradio as gr
 def speechToText(audio):
 
     # Configure the URL, see https://endpoints.ai.cloud.ovh.net/ (nvr-asr-fr-fr model)
+    # sol-stt-http-model-url
 
     # Configure header with bearer token
+    # sol-stt-http-bearer
 
     # Load and process audio file to be in the right format, see https://github.com/jiaaro/pydub/tree/master
     # Set the channel to 1
     # set the framerate to 16000
     # export the file in wav format named audio.wav
+    # sol-stt-http-audio-transform
 
     # Prepare the file to send to the endpoint
+    # sol-stt-http-file-to-send
 
     # Do the POST request and display the transcription
+    # sol-stt-http-response
     
     return None
 
 # Create a Gradio input component
+# sol-stt-http-input
 
 # Create a Gradio interface named demo
 # The function to call : reverse_audio
 # The inputs : input_audio
 # The outputs : "text"
+# sol-stt-http-demo
 
 # Main entry
 if __name__ == "__main__":
--- workshop/audio/text-to-speech-http.py
+++ workshop/audio/text-to-speech-http.py
@@ -6,8 +6,10 @@ import numpy as np
 # Function to upload or record audio, see https://www.gradio.app/main/docs/gradio/audio
 def text_to_speech(textToTransform):
     # Configure the URL, see https://endpoints.ai.cloud.ovh.net/ (nvr-tts-en-us)
+    # sol-tts-http-model-url
 
     # Configure header with bearer token
+    # sol-tts-http-bearer
 
     # Set the recognition configuration:
     # - voice_name: see https://docs.nvidia.com/deeplearning/riva/user-guide/docs/tts/tts-overview.html#pretrained-tts-models
@@ -15,22 +17,29 @@ def text_to_speech(textToTransform):
     # - encoding: 1
     # - sample_rate_hz: 16000
     # - audio_channel_count: 1
+    # sol-tts-http-payload
 
     # Send the request to endpoint with text to transform
+    # sol-tts-http-response
 
     # Return the audio
     # see https://numpy.org/doc/stable/reference/generated/numpy.frombuffer.html
+    # sol-tts-http-return
 
     return None
 
 
 # Create a Gradio input component
+# sol-tts-http-input
 
 # Create an output audio Gradio component
+# sol-tts-http-output
+
 # Create a Gradio interface named demo
 # The function to call : reverse_audio
 # The inputs : input_audio
 # The outputs : "text"
+# sol-tts-http-demo
 
 # Main entry
 if __name__ == "__main__":
