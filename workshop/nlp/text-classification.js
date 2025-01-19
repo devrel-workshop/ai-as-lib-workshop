22e479939bd3043a25456fc031fdbaee63fd64ad
philippart-s <wildagsx@gmail.com>
2025-01-19 18:24:13 +0100

    feat: ✏ NLP / JS cheat sheet

--- workshop/nlp/text-classification.js
+++ workshop/nlp/text-classification.js
@@ -6,7 +6,9 @@
 import axios from "axios";
 
 // Set the bearer token and the header to access to AI Endpoint, see https://endpoints.ai.cloud.ovh.net/
+// sol-text-class-bearer
 
 // Use Axios (see https://github.com/axios/axios) to
 //  - send a phrase to the model bert-base-NER, see https://endpoints.ai.cloud.ovh.net/
 //  - display the result (which type of texts is in the text)
+// sol-text-class-call
