2577eb9cc1c07a6a9715cbbff21f04fb32eba63e
philippart-s <wildagsx@gmail.com>
2025-02-20 17:09:40 +0100

    enhancement: ✨ JS snippets names

--- workshop/nlp/product-reviews.js
+++ workshop/nlp/product-reviews.js
@@ -6,9 +6,9 @@
 import axios from "axios";
 
 // Set the bearer token and the header to access to AI Endpoint, see https://endpoints.ai.cloud.ovh.net/
-// sol-product-bearer
+// js-06
 
 // Use Axios (see https://github.com/axios/axios) to
 //  - send a review to the model nlptown/bert-base-multilingual-uncased-sentiment, see https://endpoints.ai.cloud.ovh.net/
 //  - display the result (number of stars from one to five)
-// sol-product-call
+// js-07
--- workshop/nlp/text-classification.js
+++ workshop/nlp/text-classification.js
@@ -6,9 +6,9 @@
 import axios from "axios";
 
 // Set the bearer token and the header to access to AI Endpoint, see https://endpoints.ai.cloud.ovh.net/
-// sol-text-class-bearer
+// js-04
 
 // Use Axios (see https://github.com/axios/axios) to
 //  - send a phrase to the model bert-base-NER, see https://endpoints.ai.cloud.ovh.net/
 //  - display the result (which type of texts is in the text)
-// sol-text-class-call
+// js-05
