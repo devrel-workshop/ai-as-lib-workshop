f6e30405c4e3d2e8cc1053f829fc49ff3e99be64
Stéphane Philippart <wildagsx@gmail.com>
2025-03-12 10:59:41 +0100

    fix: 🐛 New Mistral 7B model (#27)

--- solutions/chatbot/java-langchain4j/src/main/java/com/ovhcloud/ai/langchain4j/chatbot/StreamingChatbot.java
+++ solutions/chatbot/java-langchain4j/src/main/java/com/ovhcloud/ai/langchain4j/chatbot/StreamingChatbot.java
@@ -32,9 +32,9 @@ public class StreamingChatbot {
     // Select the Mistral model to use (the streaming one)
     MistralAiStreamingChatModel steamingModel = MistralAiStreamingChatModel.builder()
         .apiKey(System.getenv("OVH_AI_ENDPOINTS_ACCESS_TOKEN"))
-        .modelName("Mistral-7B-Instruct-v0.2")
+        .modelName("Mistral-7B-Instruct-v0.3")
         .baseUrl(
-            "https://mistral-7b-instruct-v02.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1")
+            "https://mistral-7b-instruct-v0-3.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1")
         .maxTokens(512)
         .temperature(0.0)
         .logRequests(false)
