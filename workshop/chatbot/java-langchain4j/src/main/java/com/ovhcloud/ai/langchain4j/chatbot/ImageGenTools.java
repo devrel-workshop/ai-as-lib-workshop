e4dc4e03fbcf8cc740820f37444c7582f52f1da7
philippart-s <wildagsx@gmail.com>
2025-06-27 13:57:53 +0200

    feat: ✨ Add function calling workshop part

---
+++ workshop/chatbot/java-langchain4j/src/main/java/com/ovhcloud/ai/langchain4j/chatbot/ImageGenTools.java
@@ -0,0 +1,18 @@
+package com.ovhcloud.ai.langchain4j.chatbot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Here is the place where you'll add the code to generate images using Stable Diffusion XL.
+ * The steps to create your image generation tool:
+ *   - define the right Tool description thanks to the @Tool annotation from LangChain4J (https://docs.langchain4j.dev/tutorials/tools#tool)
+ *   - call Stable Diffusion XL model (see https://endpoints.ai.cloud.ovh.net/models/a363a190-ff7b-4c38-a1b9-147f9aae9328)
+ *   - generate image file with the Stable Diffusion response
+ */
+public class ImageGenTools {
+    private static final Logger _LOG = LoggerFactory.getLogger(ImageGenTools.class);
+
+    // java-24
+
+}
