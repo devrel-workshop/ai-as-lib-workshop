74972e524536a07549cd6a9a340506f22ba02e48
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-08-01 16:37:26 +0200

    feat: 🐍 Python code for NLP

---
+++ workshop/nlp/python/text-classification.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+# Use Bert-​base-​NER, see https://endpoints.ai.cloud.ovh.net/models/bert-base-ner
+# py-79
+
+# Set the bearer token and the header to access to AI Endpoint, see https://endpoints.ai.cloud.ovh.net/  
+# py-80
+
+# Call the model and display the result
+# py-81
