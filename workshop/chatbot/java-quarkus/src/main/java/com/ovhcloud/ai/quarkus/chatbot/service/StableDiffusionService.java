6655b16e09eabf88c701e1114e631895fda32f27
Stéphane Philippart <wildagsx@gmail.com>
2025-09-24 09:24:45 +0200

    doc: 📝 Add doc fr MCP with Quarkus (#60)

--- workshop/chatbot/java-quarkus/src/main/java/com/ovhcloud/ai/quarkus/chatbot/service/StableDiffusionService.java
+++ workshop/chatbot/java-quarkus/src/main/java/com/ovhcloud/ai/quarkus/chatbot/service/StableDiffusionService.java
@@ -1,5 +1,11 @@
 package com.ovhcloud.ai.quarkus.chatbot.service;
 
+/**
+ * Service to call Stable Diffusion XL model for image generation.
+ * See https://endpoints.ai.cloud.ovh.net/models/stable-diffusion-xl for more details.
+ * Use RestClient Quarkus extension.
+ * Use the {@link com.ovhcloud.ai.quarkus.chatbot.repository.SDPayload} as return type.
+ */
 // quarkus-19
 public interface StableDiffusionService {
     // quarkus-20
