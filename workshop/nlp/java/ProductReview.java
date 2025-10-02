daeb67feecc4b657b883dc8d3b49dd011213a5b2
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-10-02 17:13:59 +0200

    feat: 📝 Java NLP workshop

---
+++ workshop/nlp/java/ProductReview.java
@@ -0,0 +1,16 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 21+
+//PREVIEW
+//DEPS com.squareup.okhttp3:okhttp-jvm:5.1.0
+//DEPS com.fasterxml.jackson.core:jackson-databind:2.17.2
+
+void main() throws IOException {
+    // Initialise OkHttp client
+    // java-63
+
+    // Create the request with bearer token
+    // java-64
+
+    // Call the endpoint
+    // java-65
+}
