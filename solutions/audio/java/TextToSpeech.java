d933016ead80ef6c9380b952fe75915064389e58
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2025-10-30 10:07:30 +0100

    feat: 🎧 Audio with Javelit

--- solutions/audio/java/TextToSpeech.java
+++ solutions/audio/java/TextToSpeech.java
@@ -1,6 +1,8 @@
 /// usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS com.squareup.okhttp3:okhttp-jvm:5.1.0
+//DEPS io.javelit:javelit:0.58.0
 
+import io.javelit.core.Jt;
 import okhttp3.*;
 
 import java.io.IOException;
@@ -11,14 +13,17 @@ import java.nio.file.Path;
  * Text to Speech using NVIDIA RIVA.
  * see https://endpoints.ai.cloud.ovh.net/models/nvr-tts-en-us and https://docs.nvidia.com/deeplearning/riva/user-guide/docs/tts/tts-overview.html#pretrained-tts-models
  */
-void main() throws IOException {
-    // java-45
-    // Initialise OkHttp client
-    OkHttpClient client = new OkHttpClient();
 
-    // java-46
-    // Create JSON payload for RIVA request
-    String payload = """
+/// Text to audio encoding.
+/// @return The audio encoding in a bytes array.
+byte[] textToSpeech(String textToEncode) throws IOException {
+  // java-45
+  // Initialise OkHttp client
+  OkHttpClient client = new OkHttpClient();
+
+  // java-46
+  // Create JSON payload for RIVA request
+  String payload = """
             {
               "encoding": 1,
               "language_code": "en-US",
@@ -28,20 +33,32 @@ void main() throws IOException {
             }
             """;
 
-    // java-47
-    // Create the request with bearer token
-    RequestBody body = RequestBody.create(String.format(payload, "Hello, Wolrd!!"), MediaType.get("application/json; charset=utf-8"));
-    Request request = new Request.Builder()
-            .url("https://nvr-tts-en-us.endpoints.kepler.ai.cloud.ovh.net/api/v1/tts/text_to_audio")
-            .addHeader("Authorization", String.format("Bearer %s", System.getenv("OVH_AI_ENDPOINTS_ACCESS_TOKEN")))
-            .header("accept", "application/octet-stream")
-            .post(body)
-            .build();
+  // java-47
+  // Create the request with bearer token
+  RequestBody body = RequestBody.create(String.format(payload, textToEncode), MediaType.get("application/json; charset=utf-8"));
+  Request request = new Request.Builder()
+      .url("https://nvr-tts-en-us.endpoints.kepler.ai.cloud.ovh.net/api/v1/tts/text_to_audio")
+      .addHeader("Authorization", String.format("Bearer %s", System.getenv("OVH_AI_ENDPOINTS_ACCESS_TOKEN")))
+      .header("accept", "application/octet-stream")
+      .post(body)
+      .build();
 
-    // java-48
-    // Call the endpoint to create the audio file
-    System.out.println("⏳ Speech creation...");
-    Response response = client.newCall(request).execute();
-    Files.write(Path.of("speech.wav"), response.body().bytes());
-    System.out.println("🎵 Speech created 🎵");
+  // java-48
+  // Call the endpoint to create the audio file
+  System.out.println("⏳ Speech creation...");
+  Response response = client.newCall(request).execute();
+  //Files.write(Path.of("speech.wav"), response.body().bytes());
+  System.out.println("🎵 Speech created 🎵");
+
+  return response.body().bytes();
+}
+
+
+void main() throws IOException {
+  Jt.title("Speech to text exercise").use();
+  String englishText = Jt.textArea("🏴󠁧󠁢󠁥󠁮󠁧󠁿 󠁧󠁢English text 🏴󠁧󠁢󠁥󠁮󠁧󠁿").use();
+
+  byte[] textToSpeech = textToSpeech(englishText);
+
+  Jt.audio(textToSpeech).use();
 }
