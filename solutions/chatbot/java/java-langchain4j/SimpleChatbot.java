414081c89e78429f7be9601f90bdbf35ff46ba0f
Stéphane Philippart <wildagsx@gmail.com>
2026-02-03 16:35:39 +0100

    fix: 🐛 Code completion for Java-LangChain4J (#86)
    * doc: 📜 Autonomous Quarkus part
    
    * fix: 🐛 Unnamed class code completion

--- solutions/chatbot/java/java-langchain4j/SimpleChatbot.java
+++ solutions/chatbot/java/java-langchain4j/SimpleChatbot.java
@@ -5,46 +5,47 @@
 //DEPS ch.qos.logback:logback-classic:1.5.6
 //FILES ./resources/logback.xml
 
-import java.lang.invoke.MethodHandles;
-
-import dev.langchain4j.model.chat.ChatModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.SystemMessage;
 
-// java-02
-// AI Service to create, see https://docs.langchain4j.dev/tutorials/ai-services
-interface Assistant {
-  @SystemMessage("You are Nestor, a virtual assistant. Answer to the question.")
-  String chat(String message);
-}
+public class SimpleChatbot {
 
-private static final Logger _LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  // java-02
+  // AI Service to create, see https://docs.langchain4j.dev/tutorials/ai-services
+  interface Assistant {
+    @SystemMessage("You are Nestor, a virtual assistant. Answer to the question.")
+    String chat(String message);
+  }
 
-void main() {
-  // java-03
-  // Create a chat model using OpenAI provider
-  ChatModel chatModel = OpenAiChatModel.builder()
-      .apiKey(System.getenv("OVH_AI_ENDPOINTS_ACCESS_TOKEN"))
-      .modelName(System.getenv("OVH_AI_ENDPOINTS_MODEL_NAME"))
-      .baseUrl(System.getenv("OVH_AI_ENDPOINTS_MODEL_URL"))
-      .maxTokens(512)
-      .temperature(0.0)
-      .logRequests(false)
-      .logResponses(false)
-      .build();
+  private static final Logger _LOG = LoggerFactory.getLogger(SimpleChatbot.class);
 
-  // java-04
-  // Build the chatbot thanks to the AIService builder
-  Assistant assistant = AiServices.builder(Assistant.class)
-      .chatModel(chatModel)
-      .build();
+  public static void main(String[] args) {
+    // java-03
+    // Create a chat model using OpenAI provider
+    ChatModel chatModel = OpenAiChatModel.builder()
+        .apiKey(System.getenv("OVH_AI_ENDPOINTS_ACCESS_TOKEN"))
+        .modelName(System.getenv("OVH_AI_ENDPOINTS_MODEL_NAME"))
+        .baseUrl(System.getenv("OVH_AI_ENDPOINTS_MODEL_URL"))
+        .maxTokens(512)
+        .temperature(0.0)
+        .logRequests(false)
+        .logResponses(false)
+        .build();
 
-  // java-05
-  // Send a prompt
-  _LOG.info("💬: Question: Tell me a joke about Java developers\n");
-  _LOG.info("🤖: {}", assistant.chat("Tell me a joke about Java developers"));
+    // java-04
+    // Build the chatbot thanks to the AIService builder
+    Assistant assistant = AiServices.builder(Assistant.class)
+        .chatModel(chatModel)
+        .build();
+
+    // java-05
+    // Send a prompt
+    _LOG.info("💬: Question: Tell me a joke about Java developers\n");
+    _LOG.info("🤖: {}", assistant.chat("Tell me a joke about Java developers"));
+  }
 }
--- workshop/chatbot/java/java-langchain4j/ImageGenerationChatbot.java
+++ workshop/chatbot/java/java-langchain4j/ImageGenerationChatbot.java
@@ -7,39 +7,51 @@
 
 //SOURCES ImageGenTools.java
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Here is the place where you will add the code to create a chatbot used to generate images.
+ * Here is the place where you will add the code to create a chatbot used to
+ * generate images.
  * The steps to create your chatbot are :
  * - choose the right model on AI Endpoints
  * (https://endpoints.ai.cloud.ovh.net), we want to use Mistral 7B Instruct
  * - use the LangChain4J wrapper / lib to do the call to the model (use the AI
  * Service style),
- * - create a detailed prompt to help the model to knox that it must create a prompt and a negative prompt for Stable Diffusion
- * - use the tool from ImageGenTools to generate the image based on the created prompts by the model (see https://docs.langchain4j.dev/tutorials/tools#specifying-tools-dynamically)
+ * - create a detailed prompt to help the model to knox that it must create a
+ * prompt and a negative prompt for Stable Diffusion
+ * - use the tool from ImageGenTools to generate the image based on the created
+ * prompts by the model (see
+ * https://docs.langchain4j.dev/tutorials/tools#specifying-tools-dynamically)
  * - ask to generate an image (you can use a loop to fine tune your question).
  */
-private static final Logger _LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+public class ImageGenerationChatbot {
 
+    private static final Logger _LOG = LoggerFactory.getLogger(ImageGenerationChatbot.class);
 
-// Chatbot definition.
-// The goal of the chatbot is to build a powerful prompt for Stable diffusion XML.
-interface ChatBot {
-    // java-27
-    // Create a detailed system prompt: the goal and what the model must generate and use
+    // Chatbot definition.
+    // The goal of the chatbot is to build a powerful prompt for Stable diffusion
+    // XML.
+    interface ChatBot {
+        // java-27
+        // Create a detailed system prompt: the goal and what the model must generate
+        // and use
 
-}
+    }
 
-void main() throws Exception {
+    void main() throws Exception {
 
-    // java-28
-    // Main chatbot configuration, try to be more deterministic as possible ;)
+        // java-28
+        // Main chatbot configuration, try to be more deterministic as possible ;)
 
-    // java-29
-    // Add memory to fine tune the SDXL prompt.
+        // java-29
+        // Add memory to fine tune the SDXL prompt.
 
-    // java-30
-    // Build the chatbot thanks to LangChain4J AI Services mode (see https://docs.langchain4j.dev/tutorials/ai-services)
+        // java-30
+        // Build the chatbot thanks to LangChain4J AI Services mode (see
+        // https://docs.langchain4j.dev/tutorials/ai-services)
 
-    // java-31
-    // Start the conversation loop (enter "exit" to quit)
+        // java-31
+        // Start the conversation loop (enter "exit" to quit)
+    }
 }
