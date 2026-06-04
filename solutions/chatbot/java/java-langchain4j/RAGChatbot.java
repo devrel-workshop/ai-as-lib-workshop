ea0d0292fb51bb47d714226cae9e2938488ccfb0
Stéphane Philippart <wildagsx@gmail.com>
2026-06-04 11:59:56 +0200

    feat: 🗃️ Set RAG to Dev Quest 2026 (#112)

--- solutions/chatbot/java/java-langchain4j/RAGChatbot.java
+++ solutions/chatbot/java/java-langchain4j/RAGChatbot.java
@@ -122,8 +122,8 @@ public class RAGChatbot {
 
                 // java-23
                 // Send a prompt
-                _LOG.info("💬: Quel est le programme de Devopsdays Genève 2026?\n");
-                TokenStream tokenStream = assistant.chat("Quel est le programme de Devopsdays Genève 2026?");
+                _LOG.info("💬: Quel est le programme de Dev Quest 2026?\n");
+                TokenStream tokenStream = assistant.chat("Quel est le programme de Dev Quest 2026?");
                 CompletableFuture<ChatResponse> futureChatResponse = new CompletableFuture<>();
 
                 _LOG.info("🤖: ");
