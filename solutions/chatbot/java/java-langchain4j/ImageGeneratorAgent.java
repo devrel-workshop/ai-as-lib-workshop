1c93606bd47d213e807bfed0ec037c64c8422f7d
Stéphane Philippart <stephane.philippart@ovhcloud.com>
2026-07-23 17:26:13 +0200

    fix: 🐛 Add @V for input in VisionCritic agent

--- solutions/chatbot/java/java-langchain4j/ImageGeneratorAgent.java
+++ solutions/chatbot/java/java-langchain4j/ImageGeneratorAgent.java
@@ -100,7 +100,7 @@ public interface VisionCritic {
                 Original user request: "{{userRequest}}"
                 Please critique this image and provide a score and feedback.
                 """)
-  Critique critique(@V("userRequest") String userRequest, @UserMessage("{{imageBase64}}") ImageContent imageBase64);
+  Critique critique(@V("userRequest") String userRequest, @V("imageBase64") ImageContent imageBase64);
 }
 
 void main() {
